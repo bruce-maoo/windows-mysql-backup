72621a7f1b439083d9fe705e148bc85079ff417b
bruce-maoo <32473664+bruce-maoo@users.noreply.github.com>
2019-07-04 15:07:21 +0800

    Update testlink-db.bat

--- testlink-db.bat
+++ testlink-db.bat
@@ -1,4 +1,4 @@
 @echo off  
 set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"  
-D:\mysql-5.7.26-winx64\bin\mysqldump --opt -h 172.16.254.23  -u root -pXbosoft1 testlink1919 > D:\mysql_backup\testlink1919_%Ymd%.sql  
+D:\mysql-5.7.26-winx64\bin\mysqldump --opt -h 172.16.254.23  -u xxxx -pxxxxxx test > D:\mysql_backup\testlink1919_%Ymd%.sql  
 @echo on  
