0488130fb41f4844845e9f59ef1a5c36ad76d3fb
bruce-maoo <32473664+bruce-maoo@users.noreply.github.com>
2019-07-04 14:31:24 +0800

    Update testlink1919-db-backup.bat

--- testlink1919-db-backup.bat
+++ testlink1919-db-backup.bat
@@ -1,4 +1,26 @@
-@echo off  
-set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"  
-E:\mysql\mysql-5.7.26-winx64\bin\mysqldump --opt -h 172.16.254.23  -u root -pXbosoft1 testlink1919 > E:\testlink1919data\testlink1919_%Ymd%.sql  
-@echo on  
+@echo off
+echo.
+echo      MySQL-db-backup
+ 
+echo *****************************
+echo.
+echo taday is %date%
+echo time is %time%
+echo.
+echo *****************************
+ 
+ 
+set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"
+ 
+md "E:\testlink1919data\%Ymd%"
+ 
+"E:\mysql\mysql-5.7.26-winx64\bin\mysqldump" --opt -Q -h 172.16.254.23 -uroot -pXbosoft1 --default-character-set=utf8 testlink1919> "E:\testlink1919data\%Ymd%\testlink1919.sql"
+ 
+echo.
+ 
+echo MySQL-db-backup finish£¬please check it.
+ 
+echo.
+echo.
+ 
+pause
